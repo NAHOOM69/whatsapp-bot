f26b2a612d8f16dd7d48e8a35e0359723a03cbf5
NAHOOM69 <139867592+NAHOOM69@users.noreply.github.com>
2024-12-27 22:49:34 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,9 +1,13 @@
-// firebase.js
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  // נשים את זה בהמשך מ-Firebase Console
+  apiKey: "AIzaSyD5jf0fpog-MXZQBcCu_2D9XsrdKxuG1xk",
+  authDomain: "whatsapp-bot-97e72.firebaseapp.com",
+  projectId: "whatsapp-bot-97e72",
+  storageBucket: "whatsapp-bot-97e72.firebasestorage.app",
+  messagingSenderId: "553746243088",
+  appId:"1:553746243088:web:5cbe509ceffdd37565f495"
 };
 
 const app = initializeApp(firebaseConfig);

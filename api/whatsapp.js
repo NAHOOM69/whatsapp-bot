ae6c8c260cbe20a870c5ab4ebe3f7dc7bbf9ac85
NAHOOM69 <139867592+NAHOOM69@users.noreply.github.com>
2024-12-28 19:33:09 +0200

    Added databaseURL to Firebase configuration

--- api/whatsapp.js
+++ api/whatsapp.js
@@ -6,11 +6,13 @@ import { initializeApp, getFirestore, collection, addDoc, query, where, getDocs 
 const firebaseConfig = {
   apiKey: "AIzaSyD5jf0fpog-MXZQBcCu_2D9XsrdKxuG1xk",
   authDomain: "whatsapp-bot-97e72.firebaseapp.com",
+  databaseURL: "https://whatsapp-bot-97e72-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "whatsapp-bot-97e72",
   storageBucket: "whatsapp-bot-97e72.firebasestorage.app",
   messagingSenderId: "553746243088",
   appId: "1:553746243088:web:5cbe509ceffdd37565f495"
 };
+
 
 const firebaseApp = initializeApp(firebaseConfig);
 const db = getFirestore(firebaseApp);
--- firebase.js
+++ firebase.js
@@ -4,11 +4,13 @@ import { getFirestore } from 'firebase/firestore';
 const firebaseConfig = {
   apiKey: "AIzaSyD5jf0fpog-MXZQBcCu_2D9XsrdKxuG1xk",
   authDomain: "whatsapp-bot-97e72.firebaseapp.com",
+  databaseURL: "https://whatsapp-bot-97e72-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "whatsapp-bot-97e72",
   storageBucket: "whatsapp-bot-97e72.firebasestorage.app",
   messagingSenderId: "553746243088",
-  appId:"1:553746243088:web:5cbe509ceffdd37565f495"
+  appId: "1:553746243088:web:5cbe509ceffdd37565f495"
 };
+
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);

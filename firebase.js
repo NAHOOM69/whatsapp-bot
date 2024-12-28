347002021167d73603775549b5dbbad9fd7462f7
NAHOOM69 <139867592+NAHOOM69@users.noreply.github.com>
2024-12-28 20:12:50 +0200

    Use environment variables for Firebase configuration

--- firebase.js
+++ firebase.js
@@ -2,14 +2,14 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD5jf0fpog-MXZQBcCu_2D9XsrdKxuG1xk",
-  authDomain: "whatsapp-bot-97e72.firebaseapp.com",
-  databaseURL: "https://whatsapp-bot-97e72-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "whatsapp-bot-97e72",
-  storageBucket: "whatsapp-bot-97e72.firebasestorage.app",
-  messagingSenderId: "553746243088",
-  appId: "1:553746243088:web:5cbe509ceffdd37565f495"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 };
+
 
 
 const app = initializeApp(firebaseConfig);
